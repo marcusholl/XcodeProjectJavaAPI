73431a6d99a4a03a297485dbfd80833344c7108d
Alexander Lippling <alexander.lippling@sap.com>
2012-05-15 14:43:04 +0200

    Added JUnit
    Change-Id: Iae683d4925a484648f2137935f363aaffb75e310

---
+++ src/test/java/com/sap/tip/production/xcode/XcodeProjectParserTest.java
@@ -0,0 +1,12 @@
+package com.sap.tip.production.xcode;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class XcodeProjectParserTest {
+	@Test
+	public void success() {
+		assertTrue(true);
+	}
+}
